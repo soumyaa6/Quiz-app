e71af0b07ba9c10454935090728c113080237d8d
Soumya Hagargi <113289478+soumyaa6@users.noreply.github.com>
2023-11-28 22:11:13 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,80 @@
+const QuizData = [
+  {
+    count: 1,
+    question: "Which language runs in a web browser?",
+    options: ["Java", "C", "Python", "JavaScript"],
+    answer: 4,
+  },
+  {
+    count: 2,
+    question: "What does CSS stand for?",
+    options: [
+      "Central Style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    count: 3,
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    count: 4,
+    question: "What year was JavaScript launched?",
+    options: ["1996", "1995", "1994", "None"],
+    answer: 2,
+  },
+
+  {
+    count: 5,
+    question: "Who was the Ancient Greek God of the Sun? ",
+    options: ["Apollo", "Zues", "Thor", "None"],
+    answer: 1,
+  },
+
+  {
+    count: 6,
+    question: "What artist has the most streams on Spotify? ",
+    options: ["Drake", "Coldplay", "Maroon 5", "None"],
+    answer: 1,
+  },
+
+  {
+    count: 7,
+    question: "How many elements are in the periodic table? ",
+    options: ["117", "118", "119", "None"],
+    answer: 2,
+  },
+
+  {
+    count: 8,
+    question: "What car manufacturer had the highest revenue in 2020?",
+    options: ["Volkswagen", "Suzuki", "Toyota", "None"],
+    answer: 2,
+  },
+
+  {
+    count: 9,
+    question: "What city is known as The Eternal City?",
+    options: ["London", "Paris", "Rome", "None"],
+    answer: 3,
+  },
+
+  {
+    count: 10,
+    question: "What company was initially known as Blue Ribbon Sports?",
+    options: ["Nike", "Puma", "Adidas", "None"],
+    answer: 1,
+  },
+];
+
+export default QuizData;
